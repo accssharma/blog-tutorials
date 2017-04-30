4e311be595f3cffab7d70c9069cea8d92b601428
Ashish Sharma <accssharma@gmail.com>
2017-04-29 18:57:20 -0600

    added more comments

--- feature-extraction-caffe/extract_features_caffe.py
+++ feature-extraction-caffe/extract_features_caffe.py
@@ -8,15 +8,14 @@
 This file extracts features for input images specified as paths in a file from different layers
 in CaffeNet/Alexnet/Googlenet/VGG19 models.
 """
-# Imports
 
+# Imports
 import sys
 import pickle
 import caffe
 import numpy as np
 import csv
    
-
 # Method that returns batch of image of size "batch_size"
 def get_this_batch(image_list, batch_index, batch_size):
     start_index = batch_index * batch_size
@@ -29,11 +28,13 @@ def get_this_batch(image_list, batch_index, batch_size):
     return image_list[batch_index_indices]
 
 def main(argv):
+    # required only if working in gpu mode
     gpu_id = 4
     extract_from_layer = "fc7"
     input_exp_file = "images.txt"
     model_def = "deploy.prototxt"
     pretrained_model = "blvc_alexnet.caffemodel"
+    # output file where we want to write extracted features
     output_pkl_file_name = "out.pkl"
     batch_size = 10
     
@@ -43,35 +44,42 @@ def main(argv):
     
     caffe.set_mode_gpu();
     caffe.set_device(gpu_id); 
-
-    channel_swap_val = (2,1,0)
-    raw_scale_value = 255
     
     images_loaded_by_caffe = [caffe.io.load_image(im) for im in image_paths_list] 
     
-    net = caffe.Net(model_def, pretrained_model, caffe.TEST)
-    
-    ## Set up transformer configuration
+    # Set up transformer - creates transformer object
     transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
-    transformer.set_transpose('data', (2,0,1)) # HxWxC -> CxHxW
-    transformer.set_channel_swap('data', channel_swap_val) # RGB -> BGR (2,1,0)
-    transformer.set_raw_scale('data', raw_scale_value) 
+    # transpose image from HxWxC to CxHxW
+    transformer.set_transpose('data', (2,0,1))
+    # Swap image channels from RGB to BGR
+    transformer.set_channel_swap('data', (2,0,1))
+    # Set raw_scale = 255 to multiply with the values loaded with caffe.io.load_image
+    transformer.set_raw_scale('data', 255)
     
+    # create a net object 
+    net = caffe.Net(model_def, pretrained_model, caffe.TEST)
+	
     total_batch_nums = len(images_loaded_by_caffe)/batch_size    
     features_all_images = []
     images_loaded_by_caffe = np.array(images_loaded_by_caffe)
+    # loop through all the batches 
     for j in range(total_batch_nums+1):
 	image_batch_to_process = get_this_batch(images_loaded_by_caffe, j, batch_size)
 	num_images_being_processed = len(image_batch_to_process)
 	data_blob_index = range(num_images_being_processed)
+	# note that each batch is passed through a transformer before passing to data layer
         net.blobs['data'].data[data_blob_index] = [transformer.preprocess('data', img) for img in image_batch_to_process]
-        res = net.forward() # BEWARE: blobs arrays are overwritten
-        features_for_this_batch = net.blobs[extract_from_layer].data[data_blob_index].copy()
+        # BEWARE: blobs arrays are overwritten
+	res = net.forward()
+        # actual batch feature extraction
+	features_for_this_batch = net.blobs[extract_from_layer].data[data_blob_index].copy()
         features_all_images.extend(features_for_this_batch)
-    pkl_object = {"filename": image_paths_list, "data":features_all_images}
+    
+    # store generated features in a binarized pickle file and write to disk
+    pkl_object = {"filename": image_paths_list, "features": features_all_images}
     output = open(output_pkl_file_name, 'wb')
     pickle.dump(pkl_object, output, 2)
     output.close()
 
 if __name__ == "__main__":
-	main(sys.argv[1:])
+    main(sys.argv[1:])
